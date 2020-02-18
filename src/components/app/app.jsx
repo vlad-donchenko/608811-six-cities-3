f013f586e3d5ad02e50d1fa88b76bfb653ff88f1
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-02-18 23:01:39 +0200

    module3-task2 - Перепишет компонент на класс компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import Main from "../main/main.jsx";
 import PropTypes from "prop-types";
 
 const handleTitleClick = () => {};
 
-const App = (props) => {
-  const {offers} = props;
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <Main offers={offers} onTitleClick={handleTitleClick}/>
-  );
-};
+  render() {
+    const {offers} = this.props;
+    return (
+      <Main offers={offers} onTitleClick={handleTitleClick}/>
+    );
+  }
+}
 
 App.propTypes = {
   offers: PropTypes.arrayOf(PropTypes.shape({index: PropTypes.number.isRequired, name: PropTypes.string.isRequired})).isRequired,
