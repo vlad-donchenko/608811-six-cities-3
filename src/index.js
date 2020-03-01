a29e0bf7a2040fc747b803d47dddc02c2566dddd
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-03-01 22:18:28 +0200

    module4-task2 - Уберет console.log

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import {offers} from "./mocks/offers.js";
 
-console.log(offers);
-
 const root = document.querySelector(`#root`);
 ReactDOM.render(<App offers={offers}/>, root);
