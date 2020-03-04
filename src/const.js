1eaa502fbff1103da78a83a87592ca9dfb75b124
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-03-04 22:24:31 +0200

    module4-task2 - переименует префиксы

--- src/const.js
+++ src/const.js
@@ -3,8 +3,8 @@ const ZOOM = 12;
 const MAX_NEIGHBOURHOOD_OFFERS = 3;
 
 const OfferPrefix = {
-  MAIN_PAGE_OFFER_PREFIX: `cities__place-card`,
-  DETAILS_INFO_OFFER_PREFIX: `near-places__card`,
+  MAIN_PAGE: `cities__place-card`,
+  DETAILS_PAGE: `near-places__card`,
 };
 
 export {ZOOM, MAX_NEIGHBOURHOOD_OFFERS, OfferPrefix};
