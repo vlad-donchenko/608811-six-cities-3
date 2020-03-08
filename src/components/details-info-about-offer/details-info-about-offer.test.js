77ec0ccb77313b9d2caa87e858b988537e2b79b6
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-03-08 11:55:51 +0200

    module4-task2 - Обновит тест, сделает статичными моки

--- src/components/details-info-about-offer/details-info-about-offer.test.js
+++ src/components/details-info-about-offer/details-info-about-offer.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import DetailsInfoAboutOffer from "./details-info-about-offer.jsx";
-import {offers} from "../../mocks/offers";
+import {offers} from "../../mocks/test-offer";
 
 it(`Should render <DetailsInfoAboutOffer/>`, () => {
   const tree = renderer.create(<DetailsInfoAboutOffer offer={offers[0]} onTitleClick={() => {}}/>).toJSON();
--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -1,118 +1,174 @@
-import {getRandomInteger} from "../utils/index";
-
-const INDICATOR = 0.5;
-
-const titles = [
-  `Beautiful & luxurious apartment at great location`,
-  `Wood and stone place`,
-  `Canal View Prinsengracht`,
-  `Nice, cozy, warm big bed apartment`,
-  `Wood and stone place`
-];
-
-const images = [
-  `room.jpg`,
-  `apartment-02.jpg`,
-  `apartment-03.jpg`,
-  `apartment-01.jpg`,
-  `apartment-01.jpg`,
-];
-
-const goods = [
-  `Wi-Fi`,
-  `Heating`,
-  `Kitchen`,
-  `Fridge`,
-  `Washing machine`,
-  `Coffee machine`,
-  `Dishwasher`,
-  `Towels`,
-  `Baby seat`,
-  `Cabel TV`
-];
-
-const names = [
-  `Angelina`,
-  `Max`,
-  `Sam`,
-  `Nick`,
-  `Jane`
-];
-
-const avatars = [
-  `avatar-angelina.jpg`,
-  `avatar-max.jpg`
-];
-
-const descriptions = [
-  `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century`,
-  `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful`,
-  `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century`
-];
-
-const latitudes = [
-  52.3909553943508,
-  52.369553943508,
-  52.3909553943508,
-  52.3809553943508,
-  52.3709553943508
-];
-
-const longitude = [
-  4.85309666406198,
-  4.85309666406198,
-  4.929309666406198,
-  4.939309666406198,
-  4.979309666406198
-];
-
-const getGoods = () => goods.filter(() => Math.random() > INDICATOR);
-
-const getDescription = () => {
-  return descriptions.filter(() => {
-    return Math.random() > INDICATOR;
-  }).join(`. `);
-};
-
-const generateOffers = () => {
-  return titles.map((title, id) => {
-    return {
-      bedrooms: getRandomInteger(1, 7),
-      city: {
-        location: {
-          latitude: 52.38333,
-          longitude: 4.9,
-          zoom: 10,
-        },
-        name: `Amsterdam`
-      },
-      description: getDescription(),
-      goods: getGoods(),
-      host: {
-        avatarUrl: avatars[getRandomInteger(0, avatars.length - 1)],
-        id,
-        isPro: Math.random() > INDICATOR,
-        name: names[id]
-      },
-      id,
-      images,
-      isFavorite: Math.random() > INDICATOR,
-      isPremium: Math.random() > INDICATOR,
+const offers = [
+  {
+    bedrooms: 5,
+    city: {
       location: {
-        latitude: latitudes[id],
-        longitude: longitude[id],
-        zoom: 8
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 10
       },
-      maxAdults: getRandomInteger(1, 7),
-      previewImage: images[getRandomInteger(0, images.length - 1)],
-      price: getRandomInteger(100, 500),
-      rating: getRandomInteger(0, 5),
-      title,
-      type: Math.random() > INDICATOR ? `Private room` : `Apartment`,
-    };
-  });
-};
+      name: `Amsterdam`
+    },
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century`,
+    goods: [`Wi-Fi`, `Heating`, `Kitchen`, `Fridge`, `Washing machine`, `Baby seat`],
+    host: {
+      avatarUrl: `avatar-angelina.jpg`,
+      id: 0,
+      isPro: true,
+      name: `Angelina`
+    },
+    id: 0,
+    images: [`room.jpg`, `apartment-02.jpg`, `apartment-03.jpg`, `apartment-01.jpg`, `apartment-01.jpg`],
+    isFavorite: true,
+    isPremium: true,
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
+      zoom: 8
+    },
+    maxAdults: 3,
+    previewImage: `apartment-01.jpg`,
+    price: 494,
+    rating: 5,
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `Private room`
+  },
+  {
+    bedrooms: 1,
+    city: {
+      location: {
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 10
+      },
+      name: `Amsterdam`
+    },
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century. An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful. A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century`,
+    goods: [`Heating`, `Coffee machine`, `Dishwasher`, `Towels`, `Baby seat`, `Cabel TV`],
+    host: {
+      avatarUrl: `avatar-angelina.jpg`,
+      id: 1,
+      isPro: false,
+      name: `Max`
+    },
+    id: 1,
+    images: [`room.jpg`, `apartment-02.jpg`, `apartment-03.jpg`, `apartment-01.jpg`, `apartment-01.jpg`],
+    isFavorite: false,
+    isPremium: false,
+    location: {
+      latitude: 52.369553943508,
+      longitude: 4.85309666406198,
+      zoom: 8
+    },
+    maxAdults: 5,
+    previewImage: `apartment-01.jpg`,
+    price: 264,
+    rating: 1,
+    title: `Wood and stone place`,
+    type: `Private room`
+  },
+  {
+    bedrooms: 4,
+    city: {
+      location: {
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 10
+      },
+      name: `Amsterdam`
+    },
+    description: `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful. A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century`,
+    goods: [`Fridge`, `Dishwasher`, `Towels`],
+    host: {
+      avatarUrl: `avatar-max.jpg`,
+      id: 2,
+      isPro: false,
+      name: `Sam`
+    },
+    id: 2,
+    images: [`room.jpg`, `apartment-02.jpg`, `apartment-03.jpg`, `apartment-01.jpg`, `apartment-01.jpg`],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
+      zoom: 8
+    },
+    maxAdults: 2,
+    previewImage: `apartment-02.jpg`,
+    price: 418,
+    rating: 5,
+    title: `Canal View Prinsengracht`,
+    type: `Private room`
+  },
+  {
+    bedrooms: 7,
+    city: {
+      location: {
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 10
+      },
+      name: `Amsterdam`
+    },
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century. A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century`,
+    goods: [`Heating`, `Kitchen`, `Fridge`, `Washing machine`, `Dishwasher`, `Baby seat`, `Cabel TV`],
+    host: {
+      avatarUrl: `avatar-angelina.jpg`,
+      id: 3,
+      isPro: true,
+      name: `Nick`
+    },
+    id: 3,
+    images: [`room.jpg`, `apartment-02.jpg`, `apartment-03.jpg`, `apartment-01.jpg`, `apartment-01.jpg`],
+    isFavorite: false,
+    isPremium: false,
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 8
+    },
+    maxAdults: 7,
+    previewImage: `apartment-03.jpg`,
+    price: 358,
+    rating: 5,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `Private room`
+  },
+  {
+    bedrooms: 2,
+    city: {
+      location: {
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 10
+      },
+      name: `Amsterdam`
+    },
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    goods: [`Wi-Fi`, `Kitchen`, `Coffee machine`, `Towels`, `Baby seat`],
+    host: {
+      avatarUrl: `avatar-angelina.jpg`,
+      id: 4,
+      isPro: true,
+      name: `Jane`
+    },
+    id: 4,
+    images: [`room.jpg`, `apartment-02.jpg`, `apartment-03.jpg`, `apartment-01.jpg`, `apartment-01.jpg`],
+    isFavorite: false,
+    isPremium: false,
+    location: {
+      latitude: 52.3709553943508,
+      longitude: 4.979309666406198,
+      zoom: 8
+    },
+    maxAdults: 4,
+    previewImage: `apartment-02.jpg`,
+    price: 402,
+    rating: 5,
+    title: `Wood and stone place`,
+    type: `Apartment`
+  }
+];
 
-const offers = generateOffers();
-
-export {offers, names};
+export {offers};
